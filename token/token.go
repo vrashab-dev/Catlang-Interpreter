9996b1744503bf004e734f933ae34d8bbaeef06a
vrashab <vrashab.dev@gmail.com>
2025-03-02 12:59:46 +0530

    token.go_update_020325

--- token/token.go
+++ token/token.go
@@ -1,1 +1,42 @@
- 
+package token
+
+// TokenType represents the type of token
+type TokenType string;
+
+type Token struct {
+Type TokenType
+Literal string
+}
+
+const(
+ILLEGAL="ILLEGAL"
+EOF="EOF"
+
+// Identifiers + literals
+INDENT="INDENT"
+INT ="INT"
+
+// Operators
+ASSIGN ="="
+PLUS ="+"
+MINUS ="-"
+MUL="*"
+DIV="/"
+MOD="%%"
+
+// Delimiters
+COMMA =","
+SEMICOLON=";"
+
+LPEREN ="("
+RPEREN =")"
+LBRACE ="{"
+RBRACE ="}"
+
+// Keywords
+FUNCTION="FUNCTION"
+LET="LET"
+
+
+
+)
